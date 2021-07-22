3c0c01e17a89dea4aeb6e41201446eb7708e1790
deramchmzz <deramchmzz+github@gmail.com>
2021-07-22 16:35:27 +0900

    change: convert_to_candidates() for context, def and ref are changed.

--- rplugin/python3/denite/source/gtags_context.py
+++ rplugin/python3/denite/source/gtags_context.py
@@ -4,6 +4,7 @@ import sys
 sys.path.insert(1, os.path.dirname(__file__))
 from denite_gtags import TagsBase  # pylint: disable=locally-disabled, wrong-import-position
 
+from denite import util
 
 class Source(TagsBase):
     def __init__(self, vim):
@@ -19,3 +20,17 @@ class Source(TagsBase):
             '--from-here', '{}:{}'.format(current_line, file_name),
             '--result=ctags-mod'
         ]]
+
+    def convert_to_candidates(self, tags):
+        candidates = []
+        for tag in tags:
+            path, line, text = self._parse_tag(tag)
+            col = text.find(text) - 1
+            candidates.append({
+                'word': '{0}:{1};{2}'.format(util.abspath(self.vim, path), line, text),
+                'action__path': path,
+                'action__line': line,
+                'action__text': text,
+                'action__col': col
+            })
+        return candidates
--- rplugin/python3/denite/source/gtags_def.py
+++ rplugin/python3/denite/source/gtags_def.py
@@ -4,6 +4,7 @@ import sys
 sys.path.insert(1, os.path.dirname(__file__))
 from denite_gtags import TagsBase  # pylint: disable=locally-disabled, wrong-import-position
 
+from denite import util
 
 class Source(TagsBase):
     def __init__(self, vim):
@@ -14,3 +15,17 @@ class Source(TagsBase):
 
     def get_search_flags(self):
         return [['-d', '--result=ctags-mod']]
+
+    def convert_to_candidates(self, tags):
+        candidates = []
+        for tag in tags:
+            path, line, text = self._parse_tag(tag)
+            col = text.find(text) - 1
+            candidates.append({
+                'word': '{0}:{1};{2}'.format(util.abspath(self.vim, path), line, text),
+                'action__path': path,
+                'action__line': line,
+                'action__text': text,
+                'action__col': col
+            })
+        return candidates
